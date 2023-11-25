ce790f19864ef79494e0ba4e80c6e93d2abe8bec
Vladimir Strugov <Strugov2001@yandex.ru>
2023-11-25 09:55:37 +0300

    fix: after tests

--- controllers/users.js
+++ controllers/users.js
@@ -1,163 +1,171 @@
-import { StatusCodes } from 'http-status-codes';
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
-import User from '../models/User.js';
-import generateToken from '../utils/jwt.js';
-import { ERROR_CODE_DUPLICATE_MONGO, SALT_ROUNDS } from '../utils/constants.js';
+import { StatusCodes } from 'http-status-codes'
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+import User from '../models/User.js'
+import generateToken from '../utils/jwt.js'
+import { ERROR_CODE_DUPLICATE_MONGO, SALT_ROUNDS } from '../utils/constants.js'
 
 export const login = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
   try {
-    return User.findUserByCredentials(email, password).then((user) => {
+    return User.findUserByCredentials(email, password).then(user => {
       res.send({
-        token: generateToken({ _id: user._id }),
-      });
-    });
+        token: generateToken({ _id: user._id })
+      })
+    })
   } catch (error) {
     return res
       .status(StatusCodes.UNAUTHORIZED)
-      .send({ message: 'Неправильные почта или пароль', error: error.message });
+      .send({ message: 'Неправильные почта или пароль', error: error.message })
   }
-};
+}
 
 export const createUser = async (req, res) => {
   try {
     const newUser = await bcrypt
       .hash(req.body.password, SALT_ROUNDS)
-      .then((hash) => new User({ ...req.body, password: hash }));
+      .then(hash => new User({ ...req.body, password: hash }))
 
-    return res.status(StatusCodes.CREATED).send(await newUser.save());
+    return res
+      .status(StatusCodes.CREATED)
+      .send({
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        _id: user._id,
+        email: user.email
+      })
   } catch (error) {
     if (error instanceof mongoose.Error.ValidationError) {
       return res
         .status(StatusCodes.BAD_REQUEST)
-        .send({ message: 'Переданы неверные данные', ...error });
+        .send({ message: 'Переданы неверные данные', ...error })
     }
 
     if (error.code === ERROR_CODE_DUPLICATE_MONGO) {
       return res
         .status(StatusCodes.CONFLICT)
-        .send({ message: 'Пользователь уже существует' });
+        .send({ message: 'Пользователь уже существует' })
     }
 
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .send({ message: 'Ошибка на стороне сервера', error: error.message });
+      .send({ message: 'Ошибка на стороне сервера', error: error.message })
   }
-};
+}
 
 export const getUsers = async (req, res) => {
   try {
-    const users = await User.find({});
-    return res.send(users);
+    const users = await User.find({})
+    return res.send(users)
   } catch (error) {
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .send({ message: 'Ошибка на стороне севера', error: error.message });
+      .send({ message: 'Ошибка на стороне севера', error: error.message })
   }
-};
+}
 
 export const getCurrentUser = async (req, res) => {
   try {
-    const user = await User.findById(req.user._id).orFail();
-    return res.status(StatusCodes.OK).send(user);
+    const user = await User.findById(req.user._id).orFail()
+    return res.status(StatusCodes.OK).send(user)
   } catch (error) {
     if (error instanceof mongoose.Error.CastError) {
       return res
         .status(StatusCodes.BAD_REQUEST)
-        .send({ message: 'Переданы неверные данные', ...error });
+        .send({ message: 'Переданы неверные данные', ...error })
     }
     if (error instanceof mongoose.Error.DocumentNotFoundError) {
       return res
         .status(StatusCodes.NOT_FOUND)
-        .send({ message: 'Пользователь не найден' });
+        .send({ message: 'Пользователь не найден' })
     }
 
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .send({ message: 'Ошибка на стороне сервера', error: error.message });
+      .send({ message: 'Ошибка на стороне сервера', error: error.message })
   }
-};
+}
 
 export const getUserById = async (req, res) => {
   try {
-    const { userId } = req.params;
-    const user = await User.findById(userId).orFail();
-    return res.status(StatusCodes.OK).send(user);
+    const { userId } = req.params
+    const user = await User.findById(userId).orFail()
+    return res.status(StatusCodes.OK).send(user)
   } catch (error) {
     if (error instanceof mongoose.Error.CastError) {
       return res
         .status(StatusCodes.BAD_REQUEST)
-        .send({ message: 'Переданы неверные данные', ...error });
+        .send({ message: 'Переданы неверные данные', ...error })
     }
     if (error instanceof mongoose.Error.DocumentNotFoundError) {
       return res
         .status(StatusCodes.NOT_FOUND)
-        .send({ message: 'Пользователь не найден' });
+        .send({ message: 'Пользователь не найден' })
     }
 
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .send({ message: 'Ошибка на стороне сервера', error: error.message });
+      .send({ message: 'Ошибка на стороне сервера', error: error.message })
   }
-};
+}
 
 export const updateInfoProfile = async (req, res) => {
   try {
-    const { name, about } = req.body;
+    const { name, about } = req.body
     const updatedInfo = await User.findByIdAndUpdate(
       req.user._id,
       { name, about },
       {
         new: true,
-        runValidators: true,
-      },
-    ).orFail();
-    return res.json(updatedInfo);
+        runValidators: true
+      }
+    ).orFail()
+    return res.json(updatedInfo)
   } catch (error) {
     if (error instanceof mongoose.Error.DocumentNotFoundError) {
       return res
         .status(StatusCodes.NOT_FOUND)
-        .send({ message: 'Пользователь не найден' });
+        .send({ message: 'Пользователь не найден' })
     }
     if (error instanceof mongoose.Error.ValidationError) {
       return res
         .status(StatusCodes.BAD_REQUEST)
-        .send({ message: 'Переданы неверные данные', ...error });
+        .send({ message: 'Переданы неверные данные', ...error })
     }
 
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .send({ message: 'Ошибка на стороне севера', error: error.message });
+      .send({ message: 'Ошибка на стороне севера', error: error.message })
   }
-};
+}
 
 export const updateAvatarProfile = async (req, res) => {
   try {
-    const { avatar } = req.body;
+    const { avatar } = req.body
     const updatedInfo = await User.findByIdAndUpdate(
       req.user._id,
       { avatar },
       {
         new: true,
-        runValidators: true,
-      },
-    ).orFail();
-    return res.json(updatedInfo);
+        runValidators: true
+      }
+    ).orFail()
+    return res.json(updatedInfo)
   } catch (error) {
     if (error instanceof mongoose.Error.DocumentNotFoundError) {
       return res
         .status(StatusCodes.NOT_FOUND)
-        .send({ message: 'Пользователь не найден' });
+        .send({ message: 'Пользователь не найден' })
     }
     if (error instanceof mongoose.Error.ValidationError) {
       return res
         .status(StatusCodes.BAD_REQUEST)
-        .send({ message: 'Переданы неверные данные', ...error });
+        .send({ message: 'Переданы неверные данные', ...error })
     }
 
     return res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .send({ message: 'Ошибка на стороне севера', error: error.message });
+      .send({ message: 'Ошибка на стороне севера', error: error.message })
   }
-};
+}
