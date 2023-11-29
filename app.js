963648b4ae2265bd28d5d3bc5ab6e3ed57130a79
Vladimir Strugov <Strugov2001@yandex.ru>
2023-11-29 11:04:18 +0300

    add: limiter and helmet

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 import express, { json } from 'express';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import helmet from 'helmet';
+import { rateLimit } from 'express-rate-limit';
 import router from './routes/index.js';
 
 const { PORT = 3000, DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
-
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: 'draft-7',
+  legacyHeaders: false,
+});
 
+app.use(helmet());
+app.use(limiter);
 app.use(json());
 
 async function startApp() {
